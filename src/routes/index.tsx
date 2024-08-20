c0974ff563efc5f0fcf76279fd11c7ba83b84059
Xuan Thang <thangijr@gmail.com>
2024-08-20 11:49:44 +0700

    update base

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,18 @@
+import AboutPage from "@/pages/(website)/about/page";
 import LayoutWebsite from "@/pages/(website)/layout";
+import ProductCategory from "@/pages/(website)/product/category/page";
+import ProductDetail from "@/pages/(website)/product/id/page";
 import { Route, Routes } from "react-router-dom";
 
 const Router = () => {
     return (
         <>
             <Routes>
-                <Route path="/" element={<LayoutWebsite />}></Route>
+                <Route path="/" element={<LayoutWebsite />}>
+                    <Route path="products" element={<ProductCategory />} />
+                    <Route path="products/:id" element={<ProductDetail />} />
+                    <Route path="about" element={<AboutPage />} />
+                </Route>
             </Routes>
         </>
     );
