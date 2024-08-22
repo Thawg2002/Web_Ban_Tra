55fb5b739f9f7f062e5c2718a7f3806caf95971d
Xuan Thang <thangijr@gmail.com>
2024-08-22 20:15:08 +0700

    update homepage

--- src/assets/img.tsx
+++ src/assets/img.tsx
@@ -5,6 +5,13 @@ import banner_banh_trung_thu_5 from "./image/banner-banh-trung-thu-5c.jpg";
 import logo_traviet from "./image/logo-traviet.png";
 import logo_traviet_main from "./image/logo-traviet-alternative.png";
 import banner_qua_tet_scaled from "./image/banner-qua-tet-scaled.jpg";
+import banner_qua_tet_2022_2 from "./image/banner-qua-tet-2022-2.jpg";
+import hop_tri_an_open from "./image/hop-tri-an-open-600x600.jpg";
+import hop_tri_ki_open from "./image/hop-tri-ki-open-600x600.jpg";
+import tra_hoa_cuc from "./image/TRA-HOA-CUC-768x768.jpg";
+import tra_tuyet_zoom from "./image/tra-tuyet-zoom1.jpg";
+import zalo_image from "./image/zalo-tra-viet-qr-code.svg";
+import loai_hat_dinh_duong from "./image/5-loai-hat-dinh-duong.jpg";
 export {
     banner_qua_tang_doanh_nghiep,
     banner_am_chen,
@@ -13,4 +20,11 @@ export {
     logo_traviet,
     logo_traviet_main,
     banner_qua_tet_scaled,
+    zalo_image,
+    banner_qua_tet_2022_2,
+    hop_tri_an_open,
+    hop_tri_ki_open,
+    tra_hoa_cuc,
+    tra_tuyet_zoom,
+    loai_hat_dinh_duong,
 };
